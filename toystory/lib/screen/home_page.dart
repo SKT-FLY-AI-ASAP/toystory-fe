c0c09f05675ad9aa5d24b7beef3934d126f86157
jihye0 <llimjihye@gmail.com>
2024-08-26 18:30:56 +0900

    feat: Delete safearea (#47)

--- toystory/lib/screen/home_page.dart
+++ toystory/lib/screen/home_page.dart
@@ -7,32 +7,30 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: SafeArea(
-        child: Row(
-          children: [
-            // 사이드바
-            Expanded(
-              flex: 1,
-              child: Sidebar(), // 분리된 사이드바 위젯을 사용
-            ),
-            // 메인 컨텐츠 영역
-            Expanded(
-              flex: 3,
-              child: Padding(
-                padding: EdgeInsets.all(30),
-                child: Column(
-                  children: [
-                    // 스케치북 리스트
-                    SketchListView(),
-                    SizedBox(height: 40),
-                    // 장난감 상자 리스트
-                    ToyListView(),
-                  ],
-                ),
+      child: Row(
+        children: [
+          // 사이드바
+          Expanded(
+            flex: 1,
+            child: Sidebar(), // 분리된 사이드바 위젯을 사용
+          ),
+          // 메인 컨텐츠 영역
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: EdgeInsets.all(30),
+              child: Column(
+                children: [
+                  // 스케치북 리스트
+                  SketchListView(),
+                  SizedBox(height: 40),
+                  // 장난감 상자 리스트
+                  ToyListView(),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
